450b818ba7dd01913072b543f3acde50982066c9
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-01-26 23:33:14 +0100

    advance on dialogbubble

---
+++ GGJ - 2024/Assets/Scripts/DialoguesRelated/DialogueObject.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialogue", order = 1)]
+public class DialogueObject : ScriptableObject
+{
+    [field: Header("Dialogue options")]
+    [field: SerializeField, Tooltip("Characters per second")]
+    public float DialogueSpeed { get; private set; } = 100;
+
+    [field: SerializeField, Tooltip("Should punctuation wait be ignored ? (,?!. and - chars extends a bit the wait time after their display")]
+    public bool IgnorePunctuation { get; private set; }
+
+    [field: SerializeField, Tooltip("Should the user act to pass the dialogue ? Or auto skip to immediately read the following bubble ?")]
+    public bool AutoAdvance { get; private set; }
+
+    [field: SerializeField, Tooltip("Time after which the dialogue will auto advance.")]
+    public float AutoAdvanceTime { get; private set; }
+
+
+    [field: Header("Dialogues")]
+    [field: SerializeField, TextArea]
+    public string[] Bubbles { get; private set; }
+    public int BubblesLength => Bubbles.Length;
+
+
+    [field: Header("Follow up")]
+    [field: SerializeField, Tooltip("Does the dialogue chain up on another one ?\nUsually used to change to have different dialogue options in one go for multiple bubbles.")]
+    public DialogueObject FollowingDialogue { get; private set; }
+
+}
---
+++ GGJ - 2024/Assets/Scripts/DialoguesRelated/TypeWritterEffect.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class TypeWritterEffect : MonoBehaviour
+{
+    private DialogueObject currentDialogue;
+
+    public bool IsRunning { get; private set; }
+
+
+    private readonly Dictionary<HashSet<char>, float> punctuationWaitTimeChart = new()
+    {
+        {new HashSet<char>(){'.', '!', '?'}, 0.6f },
+        {new HashSet<char>(){',', ';', '-', ':'}, 0.3f }
+    };
+
+    private Coroutine typingCoroutine;
+
+    public void RunDialog(DialogueObject dialogue, int index, TMP_Text bubbleText)
+    {
+        if (dialogue.BubblesLength <= index)
+        {
+            Debug.LogError("Out of range dialogue requested");
+            return;
+        }
+        currentDialogue = dialogue;
+        typingCoroutine = StartCoroutine(TypeText(dialogue.Bubbles[index], bubbleText));
+    }
+
+    public void Stop()
+    {
+        StopCoroutine(typingCoroutine);
+        IsRunning = false;
+    }
+
+    private IEnumerator TypeText(string textToType, TMP_Text bubbleText)
+    {
+        IsRunning = true;
+
+        float t = 0;
+        int charIndex = 0;
+
+        while (charIndex < textToType.Length)
+        {
+            int lastCharIndex = charIndex;
+
+            t += Time.deltaTime * currentDialogue.DialogueSpeed;
+
+            charIndex = Mathf.FloorToInt(t);
+            charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
+
+            // The for loop is just for when there is multiple characters that have to show up at the same frame
+            for (int i = lastCharIndex; i < charIndex; i++)
+            {
+                bool isLast = i >= textToType.Length - 1;
+
+                bubbleText.text = textToType[..(i + 1)];
+
+                //SoundManager.Instance.PlaySound(SoundManager.Instance.text_char);
+
+                if (CheckPunctuation(textToType[i], out float waitTime) && !isLast)
+                {
+                    if (CheckPunctuation(textToType[i + 1], out float secondWaitTime))
+                    {
+                        waitTime /= 2;
+                    }
+
+                    yield return new WaitForSeconds(waitTime);
+                }
+            }
+
+            yield return null;
+        }
+
+        IsRunning = false;
+    }
+
+    private bool CheckPunctuation(char character, out float waitTime)
+    {
+        waitTime = default;
+
+        if (currentDialogue.IgnorePunctuation)
+            return false;
+
+        foreach (KeyValuePair<HashSet<char>, float> punctuationCategory in punctuationWaitTimeChart)
+        {
+            if (punctuationCategory.Key.Contains(character))
+            {
+                waitTime = punctuationCategory.Value;
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+}
