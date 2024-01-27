185130ce84cf5c552e2303ae89efbc063642e3c1
Milo Deroussi <115099699+FideleDestrier@users.noreply.github.com>
2024-01-27 21:29:29 +0100

    Plein d'ajout du premier chapitre

---
+++ GGJ - 2024/Assets/Scripts/ObjectsRelated/Door/LaunchKey.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchKey : MonoBehaviour
+{
+    private bool isLaunched;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isLaunched = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+
+        if (isLaunched && other.TryGetComponent(out DoorBehaviour door))
+        {
+            if (door.GetIsBoss())
+            {
+                other.gameObject.SetActive(false);
+            }
+
+        }
+    }
+}
