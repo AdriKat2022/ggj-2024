99f062af46c6a05a6e12f4f5d0ac451a59b81f0a
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-01-29 11:26:55 +0100

    fixed space scene

--- GGJ - 2024/Assets/Scripts/Cookie/SmoothApparition.cs
+++ GGJ - 2024/Assets/Scripts/Cookie/SmoothApparition.cs
@@ -19,7 +19,8 @@ public class SmoothApparition : MonoBehaviour
 
     void Start()
     {
-        intPos = GameManager.Instance.cookieInitPos;
+        if(GameManager.Instance != null)
+            intPos = GameManager.Instance.cookieInitPos;
         // Démarre la coroutine pour la transition
         StartCoroutine(TransitionRoutine());
     }
