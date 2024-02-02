d1513ab22373e76c68e8abe69e8ba4cb23b5e02e
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-02-02 03:22:49 +0100

    little refractor on dungeons

--- GGJ - 2024/Assets/Scripts/LevelDesign scripts/DungeonEntry.cs
+++ GGJ - 2024/Assets/Scripts/LevelDesign scripts/DungeonEntry.cs
@@ -1,33 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DungeonEntry : MonoBehaviour
 {
-    [SerializeField , Range(1, 3)] private int dungeonId;
+    private enum Dungeon{
+        TreeTemple,
+        MountTemple,
+        SpaceTemple
+    }
+
+    [SerializeField , Range(1, 3)] private Dungeon dungeonId;
 
 
+    private GameManager gameManager;
 
+
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+    }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out PlayerController player)) {
-            if (dungeonId == 1 && GameManager.Instance.dungeonState == 0)
+
+
+            switch (dungeonId)
             {
-                GameManager.Instance.dungeonState = 1;
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            }
-            else if (dungeonId == 2 && GameManager.Instance.dungeonState == 2)
-            {
-                GameManager.Instance.dungeonState = 3;
-                SceneManager.LoadScene("MountTemple");
-            }
-            else if (dungeonId == 3 && GameManager.Instance.dungeonState == 4)
-            {
-                GameManager.Instance.dungeonState = 5;
-                SceneManager.LoadScene("SpaceTemple");
+                case Dungeon.TreeTemple:
+
+                    if (gameManager.dungeonState != 0)
+                        return;
+                    gameManager.dungeonState = 1;
+                    SceneManager.LoadScene("TreeTemple");
+
+                    break;
+
+                case Dungeon.MountTemple:
+
+                    if (gameManager.dungeonState != 2)
+                        return;
+                    gameManager.dungeonState = 3;
+                    SceneManager.LoadScene("MountTemple");
+
+                    break;
+
+                case Dungeon.SpaceTemple:
+
+                    if (gameManager.dungeonState != 4)
+                        return;
+                    gameManager.dungeonState = 5;
+                    SceneManager.LoadScene("SpaceTemple");
+
+                    break;
             }
         }
     }
